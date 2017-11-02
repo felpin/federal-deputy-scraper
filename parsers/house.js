b624a5e73198e8ca6f517c24b7ccbe41d1bacbc2
Felipe Pinheiro <felipe.pinheiro.90@hotmail.com>
2017-11-02 09:14:08 -0200

    Add anothers extract functions

--- parsers/house.js
+++ parsers/house.js
@@ -1,5 +1,7 @@
 const cheerio = require('cheerio');
 
+const extractInformation = (node, child = 1) => node.children[child].data.trim();
+
 const extractDeputies = (html) => {
   const $ = cheerio.load(html);
 
@@ -18,6 +20,42 @@ const extractDeputies = (html) => {
     .get();
 };
 
+const extractDeputyInformations = (html) => {
+  const $ = cheerio.load(html);
+  const informations = $('ul.visualNoMarker').children();
+
+  const fullName = extractInformation(informations[0]);
+  const birthday = extractInformation(informations[1]).replace(' / ', '/');
+  const [party, state, type] = extractInformation(informations[2]).split(' / ');
+  const main = type === 'Titular';
+
+  const phoneAndFax = informations[3];
+  let phone = extractInformation(phoneAndFax);
+  phone = phone.substring(0, phone.length - 2);
+  const fax = extractInformation(phoneAndFax, 3);
+
+  const legislatures = extractInformation(informations[4]).split(' ');
+
+  return {
+    fullName,
+    birthday,
+    party,
+    state,
+    main,
+    phone,
+    fax,
+    legislatures,
+  };
+};
+
+const extractDeputyProfilePictureUrl = (html) => {
+  const $ = cheerio.load(html);
+
+  return $('img.image-left')[0].attribs.src;
+};
+
 module.exports = {
   extractDeputies,
+  extractDeputyInformations,
+  extractDeputyProfilePictureUrl,
 };
